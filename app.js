1cf223277517337a243d6c792dce5c679f24c52f
Kaleidoscopee <133397937+Kaleidoscopee@users.noreply.github.com>
2023-06-16 17:51:56 -0400

    Final Commit

--- app.js
+++ app.js
@@ -123,16 +123,232 @@ for(let i=12; i <= 4000; i+=3) {
 }
 
 //Get Even
-//for(let i = 1; <)
-
+for (let i = 1; i<= 100; i++) {
+   if (i % 2 === 0) {
+      console.log( i + "<-- is an even number")
+   }
+}
 
 //Give Me 5
+for (let i = 0; i<= 100; i++) {
+   if (i % 5 === 0) {
+      console.log("I found a " + i + ". High five!")
+   }
+   if (i % 3 === 0) {
+      console.log("I found a " + i + ". Three is a crowd")
+   }
+}
 
 //Savings Account
+let bank_account = 0;
+for(let i = 1; i <=100; i++) {
+   bank_account += i;
+}
+
+console.log("My bank account this week has " + bank_account + " dollars!");
+
+for(let i = 1; i <=100; i++) {
+   bank_account += i * 2;
+}
+
+console.log("My bank account for double pay has " + bank_account + " dollars!");
 
 //ARRAYS & CONTROL FLOW
+/* A. Talk about it:
+   What are the things in an array called?
+      The thiings inside an array are called an item or element.
+
+   Do Arrays guarantee those things will be in order?
+      Arrays will be in order, unless you make them otherwise. 
+
+   What real-life thing could you model with an array?
+      You could array or list out the jewelry in a treasure chest
+      with each of it's indexed spot with the first spot being
+      index 0.
+
+*/ 
+
+//B. Easy Does It
+let quotes = ["What goes up must come down.","To be or not to be.","Life's like a box of chocolates."];
+
+//C. Accessing elements
+const randomThings = [ 1, 10, "Hello", true]
+console.log(randomThings[0]); //1st element is index 0
+randomThings[2] = "World" //String value World put into index 2 of array randomThings
+console.log(randomThings); //make sure it updated to world
+
+//D. Change Values
+const ourClass = ["Salty", "Zoom", "Sardine", "Slack", "Github"]
+console.log(ourClass[2]); //Accesses 3rd element [index 2]
+ourClass[4] = "Octocat"; //Accesses 5th element [index 4] and assigns "Octocat"
+ourClass.push("Cloud City"); //adds Cloud City to end of array using .push 
+
+//E. Mix It Up
+const myArray = [5, 10, 500, 20] //array given
+myArray.push("Aegon"); //add Aegon to the end using .push
+myArray.push("Jeff"); //add your own "" to the end using .push
+myArray.shift(); // get rid of 5 at the beginning using .shift
+myArray.unshift("Bob Marley"); // Add Bob Marley to beggining using .unshift
+myArray.pop(); //gets rid of element at the end using .pop 
+myArray.reverse(); //flips the arrays order using .reverse
+
+/*
+Did you mutate the array?
+I did mutate the array.
+
+What does mutate mean?
+Mutate is when you change the original array.
+
+Did the .reverse()method return anything?
+It returns the array, but the elements are flipped, so the first is now the last and so on. 
+*/ 
+
+//F. Biggie Smalls
+let numF = 110; //value of variable = 110
+
+if(numF < 100) {
+   console.log("little number"); // under 100 = "little number"
+} else {
+   console.log("big number"); // else print "big number"
+}
+
+//G. Monkey in the Middle
+let numG = 7; //value of variable = 7 should print "monkey"
+
+if(numG < 5) {
+   console.log("little number"); // under 100 = "little number"
+} else if(numG > 10) {
+   console.log("big number"); // (else if) print "big number"
+} else {
+   console.log("monkey"); // else print "monkey"
+}
+
+//H. What's in your closet
+
+const kristynCloset = [
+   "left shoe",
+   "cowboy shoe",
+   "right sock",
+   "GA hoodie",
+   "green pants",
+   "yellow knit hat",
+   "marshmallow peeps"
+];
+
+
+// {
+//    "firstName": "John",
+//    "lastName": "Smith",
+//    "age": 25
+// }
+//  I'm not sure why the above code is in the read.me?
+
+
+
+const thomsCloset = [
+   [
+      //Thom's shirts
+      "grey button-up",
+      "dark grey button-up",
+      "light blue button-up",
+      "blue button-up",
+   ],[
+      //Thom's pants
+      "grey jeans",
+      "jeans",
+      "PJs"
+   ],[
+      //Thom's accessories
+      "wool mittens",
+      "wool scarf",
+      "raybans"
+   ]
+];
+
+//What's Kristyn wearing today?
+console.log("Kristyn is rocking that " + kristynCloset[2] + " today!") //3rd thing in Kristyn's closet
+kristynCloset.splice(6, 0, "raybans"); //puts raybans at index 6 using .splice
+kristynsCloset[5] = "stained knit hat"; //element 6 or index 5 is now "stained..."
+
+//Thom's array
+//3. Thom's outfit
+let shirtThom = thomsCloset[0][3]; //the item accessed was Thom's "blue button-up" shirt
+let pantsThom = thomsCloset[1][1]; //"jeans"
+let accesoryThom = thomsCloset[2][2]; //"raybans"
+
+console.log("Thom is rocking it wearing his " + shirtThom + ", " + pantsThom + ", and " + accesoryThom + " to block out the haters! B)")
+
+
+thomsCloset[1][2] = "Footie Pajamas";
+console.log(thomasCloset[1][2]);
+
+
+
 
 //IV. FUNCTIONS
+
+//A. printGreeting
+
+function printGreeting(name) {
+   return "Hello there, " + name + "!";
+}
+
+console.log(printGreeting("Jose")); //Plugs my name into the parameter for the greeting.
+
+//B. printCool
+
+function printCool (name) {
+   return name + "is cool";
+}
+
+console.log(printCool("Jose")); // same as above but now for this phrase
+
+//C. Calculate Cube (volume of cube = a^3)
+
+function calculateCube (num) {
+   return num ** 3;
+}
+
+console.log(calculateCube(3)); //should be 3^3=27
+
+//D. isVowel
+
+function isVowel (letter) {
+   let vowel = ['A','a','E','e','I','i','O','o','U','u'];
+   for(let i = 0; i< vowel.length; i++) {
+      if (letter === vowel[i]) {
+         return true;
+      }
+   }
+   return false;
+}
+
+console.log(isVowel("A"));
+console.log(isVowel("z"));
+console.log(isVowel("u"));
+
+//E. getTwoLengths
+
+function getTwoLengths(str1, str2) {   //string 1 and string 2 set in parameter
+   return [str1.length, str2.length];  //sting 1 and string 2 in an array
+}
+
+console.log(getTwoLengths("Dapper","Linguistic")); //should be [6,10]
+
+
+//F. getMultipleLegths
+
+function getMultipleLegths(string) {
+   let numberOfLetters = []; // making the array to hold  future string input
+   for(let i = 0; i < string.length; i++) {  //for loop to count the string
+      numberOfLetters.push(string[i].length); //using .push to push the count of the string to the end of the array
+   }
+   return numberOfLetters;   //prints the array once all the strings are counted
+}
+
+console.log(getMultipleLegths(["Whatsup Doc?","Yes","amazing","cool","Tree"])); //should be [12, 3, 7, 4, 4]
+
+
 
 // G. maxOfThree
 function maxOfThree (num1,num2,num3) {
